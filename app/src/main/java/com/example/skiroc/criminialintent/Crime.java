551b6d7ba5a17b9da4eb68d4659af4ed64dc0a84
cskiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-01-26 15:09:05 -0500

    Creating Crime class

---
+++ app/src/main/java/com/example/skiroc/criminialintent/Crime.java
@@ -0,0 +1,18 @@
+package com.example.skiroc.criminialintent;
+
+import java.util.UUID;
+
+/**
+ * Created by skiroc on 1/26/17.
+ */
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        // Generate unique identifier
+        mId = UUID.randomUUID();
+    }
+}
