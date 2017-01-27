925a3296318a359be5429a4502479cc67298ee82
cskiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-01-27 13:05:05 -0500

    Overriding the onCreateView method

--- app/src/main/java/com/example/skiroc/criminialintent/CrimeFragment.java
+++ app/src/main/java/com/example/skiroc/criminialintent/CrimeFragment.java
@@ -2,18 +2,49 @@ package com.example.skiroc.criminialintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by skiroc on 1/27/17.
+ *
+ * Controller that interacts with model and view objects
+ * Presents and updates the details of a specific crime
  */
 
 public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
 
+    /**
+     * Fragment lifecycle methods must be public
+     * because they will be called by the activity
+     * hosting the fragment
+     */
+
+    /**
+     * Configuring the fragment instance
+     * @param savedInstanceState
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mCrime = new Crime();
+    }
+
+    /**
+     * Inflates the layout of the fragment's view
+     *
+     * @param inflater
+     * @param container
+     * @param savedInstanceState Contains the data to recreate the view from a saved state
+     * @return View
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime, container, false);
+        return view;
     }
 }
