6352f819ef5137d0f3d44c5e173011fc06945ebf
cskiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-02-06 05:49:22 -0500

    Generating crimes

--- app/src/main/java/com/example/skiroc/criminialintent/CrimeLab.java
+++ app/src/main/java/com/example/skiroc/criminialintent/CrimeLab.java
@@ -24,6 +24,13 @@ public class CrimeLab {
 
     private CrimeLab(Context context) {
         mCrimes = new ArrayList<>();
+        // Generating dummy crimes
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0); //Every other one
+            mCrimes.add(crime);
+        }
     }
 
     public List<Crime> getCrimes() {
