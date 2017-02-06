bbdabc8f2fe4520ed78387eb8b408e35e0296c59
cskiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-02-06 06:07:54 -0500

    Creating CrimeListFragment

---
+++ app/src/main/java/com/example/skiroc/criminialintent/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package com.example.skiroc.criminialintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by skiroc on 2/6/17.
+ */
+
+public class CrimeListFragment extends Fragment {
+}
