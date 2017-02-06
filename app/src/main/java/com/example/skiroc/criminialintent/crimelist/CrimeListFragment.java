d0d64277ef0763d34063255df7d64fe55bbd479f
cskiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-02-06 07:17:47 -0500

    Adding logic to CrimeAdapter to create and bind a ViewHolder

--- app/src/main/java/com/example/skiroc/criminialintent/crimelist/CrimeListFragment.java
+++ app/src/main/java/com/example/skiroc/criminialintent/crimelist/CrimeListFragment.java
@@ -53,19 +53,38 @@ public class CrimeListFragment extends Fragment {
             mCrimes = crimes;
         }
 
+        /**
+         * Called by the RecyclerView when it needs
+         * a new View to display an item
+         *
+         * @param parent
+         * @param viewType
+         * @return CrimeHolder
+         */
         @Override
         public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return null;
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater
+                    .inflate(android.R.layout.simple_list_item_1, parent, false);
+            return new CrimeHolder(view);
         }
 
+        /**
+         * Binds a ViewHolder's View to your model object
+         * Receives the ViewHolder and the position in your data set
+         * 
+         * @param holder
+         * @param position
+         */
         @Override
         public void onBindViewHolder(CrimeHolder holder, int position) {
-
+            Crime crime = mCrimes.get(position);
+            holder.mTitleTextView.setText(crime.getTitle());
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return mCrimes.size();
         }
     }
 }
