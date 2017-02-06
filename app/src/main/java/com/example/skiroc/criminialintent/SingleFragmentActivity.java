a0843fc631f7ef60c4c4830fd1669a18eb71b416
cskiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-02-06 05:55:21 -0500

    Creating an abstract activity

---
+++ app/src/main/java/com/example/skiroc/criminialintent/SingleFragmentActivity.java
@@ -0,0 +1,10 @@
+package com.example.skiroc.criminialintent;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by skiroc on 2/6/17.
+ */
+
+public abstract class SingleFragmentActivity extends FragmentActivity {
+}
