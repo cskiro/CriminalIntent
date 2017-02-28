beeec93522e3daa3b6ffdec2f96abb6f9bcd770d
cskiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-02-28 06:11:48 -0500

    Setting up ViewPager

--- app/src/main/java/com/example/skiroc/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/example/skiroc/criminalintent/CrimePagerActivity.java
@@ -1,8 +1,19 @@
 package com.example.skiroc.criminalintent;
 
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.example.skiroc.criminialintent.R;
+
 /**
  * Created by skiroc on 2/28/17.
  */
 
-public class CrimePagerActivity {
+public class CrimePagerActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+    }
 }
