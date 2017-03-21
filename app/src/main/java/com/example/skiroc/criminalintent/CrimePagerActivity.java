7a22b71bc07cd03bb22617feebc30020ad4f1da6
cskiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-03-21 05:52:45 -0400

    Creating newIntent for CrimePagerActivity

--- app/src/main/java/com/example/skiroc/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/example/skiroc/criminalintent/CrimePagerActivity.java
@@ -1,5 +1,7 @@
 package com.example.skiroc.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -13,20 +15,32 @@ import com.example.skiroc.criminalintent.model.CrimeLab;
 import com.example.skiroc.criminialintent.R;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by skiroc on 2/28/17.
  */
 
 public class CrimePagerActivity extends FragmentActivity {
+    private static final String EXTRA_CRIME_ID =
+            "com.example.skiroc.criminalintent.crime_id";
 
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
+
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
+
+        UUID crimeId = (UUID) getIntent()
+                .getSerializableExtra(EXTRA_CRIME_ID);
 
         // Finding ViewPager in the activity's view
         mViewPager = (ViewPager) findViewById(R.id.activity_crime_view_pager);
