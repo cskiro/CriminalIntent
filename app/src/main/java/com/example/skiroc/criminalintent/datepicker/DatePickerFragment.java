11cbe3b0f6a4a5d3709130a982d73991ca5d3dc2
cskiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-03-31 19:09:59 -0400

    Creating DatePicker Dialog Fragment

--- app/src/main/java/com/example/skiroc/criminalintent/datepicker/DatePickerFragment.java
+++ app/src/main/java/com/example/skiroc/criminalintent/datepicker/DatePickerFragment.java
@@ -1,10 +1,22 @@
 package com.example.skiroc.criminalintent.datepicker;
 
+import android.app.Dialog;
+import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+import com.example.skiroc.criminialintent.R;
 
 /**
  * Created by skiroc on 3/31/17.
  */
 
 public class DatePickerFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
 }
