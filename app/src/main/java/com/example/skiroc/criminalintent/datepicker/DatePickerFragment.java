76b0b25da4015079b39322abd97c2ef6c528e77f
cskiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-03-31 19:41:50 -0400

    Setting the view to display the Date Picker

--- app/src/main/java/com/example/skiroc/criminalintent/datepicker/DatePickerFragment.java
+++ app/src/main/java/com/example/skiroc/criminalintent/datepicker/DatePickerFragment.java
@@ -4,6 +4,8 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import com.example.skiroc.criminialintent.R;
 
@@ -12,9 +14,13 @@ import com.example.skiroc.criminialintent.R;
  */
 
 public class DatePickerFragment extends DialogFragment {
+    View mView = LayoutInflater.from(getActivity())
+            .inflate(R.layout.dialog_date, null);
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         return new AlertDialog.Builder(getActivity())
+                .setView(mView)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
