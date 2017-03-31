401e51795c0f206f3dba947d6a4fef657b7acbb7
cskiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-03-31 18:49:14 -0400

    Create DatePickerFragment class

---
+++ app/src/main/java/com/example/skiroc/criminalintent/datepicker/DatePickerFragment.java
@@ -0,0 +1,10 @@
+package com.example.skiroc.criminalintent.datepicker;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by skiroc on 3/31/17.
+ */
+
+public class DatePickerFragment extends DialogFragment {
+}
