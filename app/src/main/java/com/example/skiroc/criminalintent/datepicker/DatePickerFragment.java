49981cd19e77e84461ac1af4aebfd1c86480ccde
cskiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-03-31 19:44:51 -0400

    Adding newInstance method to DatePickerFragment

--- app/src/main/java/com/example/skiroc/criminalintent/datepicker/DatePickerFragment.java
+++ app/src/main/java/com/example/skiroc/criminalintent/datepicker/DatePickerFragment.java
@@ -6,14 +6,30 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
 
 import com.example.skiroc.criminialintent.R;
+
+import java.util.Date;
 
 /**
  * Created by skiroc on 3/31/17.
  */
 
 public class DatePickerFragment extends DialogFragment {
+
+    private static final String ARG_DATE = "date";
+
+    private DatePicker mDatePicker;
+
+    public static DatePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE, date);
+
+        DatePickerFragment datePickerFragment = new DatePickerFragment();
+        datePickerFragment.setArguments(args);
+        return datePickerFragment;
+    }
     View mView = LayoutInflater.from(getActivity())
             .inflate(R.layout.dialog_date, null);
 
