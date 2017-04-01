9077c0e62067649c1dab5176fbd774d6c071fb70
cskiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-03-31 20:14:08 -0400

    Retrieving the extra and setting the date on the Crime

--- app/src/main/java/com/example/skiroc/criminalintent/crime/CrimeFragment.java
+++ app/src/main/java/com/example/skiroc/criminalintent/crime/CrimeFragment.java
@@ -1,5 +1,7 @@
 package com.example.skiroc.criminalintent.crime;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -140,5 +142,19 @@ public class CrimeFragment extends Fragment {
         });
 
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+
+        if (requestCode == REQUEST_DATE) {
+            Date date = (Date) data
+                    .getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mCrime.setDate(date);
+            mDateButton.setText(mCrime.getDate().toString());
+        }
     }
 }
