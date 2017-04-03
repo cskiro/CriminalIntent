1579123b2fd134103c4a03ad03a6de90605b39cf
cskiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-04-03 05:41:00 -0400

    Creating Db Schema and defining table Crime

---
+++ app/src/main/java/com/example/skiroc/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,11 @@
+package com.example.skiroc.criminalintent.database;
+
+/**
+ * Created by skiroc on 4/3/17.
+ */
+
+public class CrimeDbSchema {
+    public static final class CrimeTable {
+        public static final String NAME = "crimes";
+    }
+}
