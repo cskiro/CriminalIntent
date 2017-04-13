9112db45e3c8c00dc2f1d4ff5224b995b28a08c3
cskiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-04-13 05:28:04 -0400

    Stripping out the list of crimes

--- app/src/main/java/com/example/skiroc/criminalintent/model/CrimeLab.java
+++ app/src/main/java/com/example/skiroc/criminalintent/model/CrimeLab.java
@@ -16,7 +16,6 @@ import java.util.UUID;
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
 
-    private List<Crime> mCrimes;
     private Context mContext;
     private SQLiteDatabase mDatabase;
 
@@ -30,27 +29,17 @@ public class CrimeLab {
     private CrimeLab(Context context) {
         mContext = context.getApplicationContext();
         mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
-        mCrimes = new ArrayList<>();
     }
 
     public void addCrime(Crime crime) {
-        mCrimes.add(crime);
+
     }
 
     public void deleteCrime(Crime crime) {
-        mCrimes.remove(crime);
     }
 
-    public List<Crime> getCrimes() {
-        return mCrimes;
-    }
 
     public Crime getCrime(UUID id) {
-        for (Crime crime : mCrimes) {
-            if (crime.getId().equals(id)) {
-                return crime;
-            }
-        }
         return null;
     }
 }
