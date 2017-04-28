88d67721be0b79f5f719b139e856c6a45e846428
Skiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-04-28 11:19:21 -0400

    Adding getCrime method to CursorWrapper class

--- app/src/main/java/com/example/skiroc/criminalintent/database/CrimeCursorWrapper.java
+++ app/src/main/java/com/example/skiroc/criminalintent/database/CrimeCursorWrapper.java
@@ -13,4 +13,13 @@ public class CrimeCursorWrapper extends CursorWrapper {
     public CrimeCursorWrapper(Cursor cursor) {
         super(cursor);
     }
+
+    public Crime getCrime() {
+        String uuidString = getString(getColumnIndex(CrimeDbSchema.CrimeTable.Columns.UUID));
+        String title = getString(getColumnIndex(CrimeDbSchema.CrimeTable.Columns.TITLE));
+        long date = getLong(getColumnIndex(CrimeDbSchema.CrimeTable.Columns.DATE));
+        int isSolved = getInt(getColumnIndex(CrimeDbSchema.CrimeTable.Columns.SOLVED));
+
+        return null;
+    }
 }
