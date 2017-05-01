28f4cfaeec79133c9964837c4bb855fafd4d5548
Skiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-04-30 22:58:32 -0400

    Creating method to find photo file location

--- app/src/main/java/com/example/skiroc/criminalintent/model/CrimeLab.java
+++ app/src/main/java/com/example/skiroc/criminalintent/model/CrimeLab.java
@@ -4,11 +4,13 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Environment;
 
 import com.example.skiroc.criminalintent.database.CrimeBaseHelper;
 import com.example.skiroc.criminalintent.database.CrimeCursorWrapper;
 import com.example.skiroc.criminalintent.database.CrimeDbSchema;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -43,13 +45,13 @@ public class CrimeLab {
 
     public void deleteCrime(UUID id) {
         mDatabase.delete(CrimeDbSchema.CrimeTable.NAME, CrimeDbSchema.CrimeTable.Columns.UUID
-                        + " = ?", new String[] { id.toString() });
+                + " = ?", new String[]{id.toString()});
     }
 
     public Crime getCrime(UUID id) {
         CrimeCursorWrapper cursor = queryCrimes(
                 CrimeDbSchema.CrimeTable.Columns.UUID + " = ?",
-                new String[] { id.toString() }
+                new String[]{id.toString()}
         );
 
         try {
@@ -63,7 +65,6 @@ public class CrimeLab {
             cursor.close();
         }
     }
-
 
     public List<Crime> getCrimes() {
         List<Crime> crimes = new ArrayList<>();
@@ -89,6 +90,17 @@ public class CrimeLab {
         mDatabase.update(CrimeDbSchema.CrimeTable.NAME, values,
                 CrimeDbSchema.CrimeTable.Columns.UUID + " = ?",
                 new String[]{uuidString});
+    }
+
+    public File getPhotoFile(Crime crime) {
+        File externalFilesDir = mContext
+                .getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        if (externalFilesDir == null) {
+            return null;
+        }
+
+        return new File(externalFilesDir, crime.getPhotoFilename());
     }
 
     private static ContentValues getContentValues(Crime crime) {
