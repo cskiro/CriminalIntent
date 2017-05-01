3d9e2cfe6af38c277381ab4836480ce976481ac9
Skiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-05-01 09:06:48 -0400

    Moving CrimePagerActivity to crime pager package

--- app/src/main/java/com/example/skiroc/criminalintent/crimepager/CrimePagerActivity.java
+++ app/src/main/java/com/example/skiroc/criminalintent/crimepager/CrimePagerActivity.java
@@ -1,10 +1,9 @@
-package com.example.skiroc.criminalintent;
+package com.example.skiroc.criminalintent.crimepager;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
