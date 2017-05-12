f6c980a0e0f5f64d01fe18e1b5cd389d94bbcccc
Skiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-05-12 13:34:08 -0400

    Changing to two-pane layout structure

--- app/src/main/java/com/example/skiroc/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/example/skiroc/criminalintent/SingleFragmentActivity.java
@@ -18,7 +18,7 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     @LayoutRes
     protected int getLayoutResId() {
-        return R.layout.activity_fragment;
+        return R.layout.activity_twopane;
     }
 
     @Override
--- app/src/main/java/com/example/skiroc/criminalintent/crimelist/CrimeListActivity.java
+++ app/src/main/java/com/example/skiroc/criminalintent/crimelist/CrimeListActivity.java
@@ -3,6 +3,7 @@ package com.example.skiroc.criminalintent.crimelist;
 import android.support.v4.app.Fragment;
 
 import com.example.skiroc.criminalintent.SingleFragmentActivity;
+import com.example.skiroc.criminialintent.R;
 
 /**
  * Created by skiroc on 2/6/17.
@@ -13,5 +14,10 @@ public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
         return new CrimeListFragment();
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_twopane;
     }
 }
