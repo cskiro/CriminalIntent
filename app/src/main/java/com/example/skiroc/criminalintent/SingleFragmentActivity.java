8b3b9ea5465ff4345fd40d70807c515cca5f7096
Skiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-05-12 13:15:39 -0400

    Getting a resource Id to return more than one layout

--- app/src/main/java/com/example/skiroc/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/example/skiroc/criminalintent/SingleFragmentActivity.java
@@ -1,6 +1,7 @@
 package com.example.skiroc.criminalintent;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -13,13 +14,17 @@ import com.example.skiroc.criminialintent.R;
  */
 
 public abstract class SingleFragmentActivity extends AppCompatActivity {
-
     protected abstract Fragment createFragment();
+
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
+        setContentView(getLayoutResId());
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
